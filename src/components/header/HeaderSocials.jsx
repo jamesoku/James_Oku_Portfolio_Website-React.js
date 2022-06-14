d74a4341f4bce7a980366825edd5022942eb4e54
james202 <okujames7@gmail.com>
2022-06-14 00:23:22 -0600

    finished header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,11 +4,9 @@ import {AiFillGithub} from 'react-icons/ai'
 
 const HeaderSocials = () => {
   return (
-      
      <div className='header__socials'>
-         <div>ys</div>
-         <a href="https://www.linkedin.com/in/james-nnanna-oku/" target="_blank" rel="noreferrer">{BsLinkedin}</a>
-         <a href="https://github.com/jamesoku" target="_blank" rel="noreferrer">{AiFillGithub}</a>
+        <span className='first'> <a href="https://www.linkedin.com/in/james-nnanna-oku/" target="_blank" ><BsLinkedin/></a></span>
+        <div><a href="https://github.com/jamesoku" target="_blank"><AiFillGithub/></a></div>
      </div>
   )
 }
--- src/components/header/cta.jsx
+++ src/components/header/cta.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/cv.pdf'
 const cta = () => {
   return (
     <div className='cta'>
-        <a href={CV}download className='btn'> Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <a className="btn down" href={CV}download>Download CV</a>
+        <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
   )
 }
