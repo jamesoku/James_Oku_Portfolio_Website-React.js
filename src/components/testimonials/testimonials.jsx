45e011ac58cb80feaab2c622ff70db93b4dfff42
James Oku <104051229+jamesoku@users.noreply.github.com>
2022-09-23 10:56:06 -0600

    new

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -8,7 +8,7 @@ const testimonials = () => {
       <div className='container testimonials__container'>
         <article className='testimonial'>
           <div className='client__avatar'>
-            <img src="https://media-exp2.licdn.com/dms/image/C5603AQFGppgIWkAdCg/profile-displayphoto-shrink_400_400/0/1649370200092?e=1661385600&v=beta&t=Ol02Qu_P8Ydf9oNFm49tzf7NsNkRVGeCkRQG26J1Qo4" alt="" />
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQFGppgIWkAdCg/profile-displayphoto-shrink_400_400/0/1649370200092?e=1669248000&v=beta&t=asyK1TsnA2jtD5ovqOqg5LIF9LdL8nDHWzlOCM7ZSqQ" alt="" />
           </div>
           <h5>Christopher Liechty</h5>
             <small className='client__review'>
