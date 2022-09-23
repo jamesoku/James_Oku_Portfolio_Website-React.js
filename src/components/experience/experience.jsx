8414fbc932f829a703796693260930da95d4515f
James Oku <104051229+jamesoku@users.noreply.github.com>
2022-09-23 10:48:39 -0600

    added new fonts

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,6 +1,29 @@
 import React from 'react'
 import'./experience.css'
 import {GoVerified} from 'react-icons/go'
+import {ImHtmlFive} from 'react-icons/im'
+import {SiCss3} from 'react-icons/si'
+import {SiJavascript} from 'react-icons/si'
+import {SiPython} from 'react-icons/si'
+import {SiCplusplus} from 'react-icons/si'
+import {SiKotlin} from 'react-icons/si'
+import {TbBrandPhp} from 'react-icons/tb'
+import {SiTypescript} from 'react-icons/si'
+import {SiAngular} from 'react-icons/si'
+import {SiReact} from 'react-icons/si'
+import {SiGit} from 'react-icons/si'
+import {SiGithub} from 'react-icons/si'
+import {SiNodedotjs} from 'react-icons/si'
+import {SiDocker} from 'react-icons/si'
+import {SiJquery} from 'react-icons/si'
+
+
+
+
+
+
+
+
 
 const experience = () => {
   return (
@@ -13,7 +36,7 @@ const experience = () => {
           <h3>Languages</h3>
           <div className='experience__content2'>
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <ImHtmlFive className='experience__details-icon'/>
           <div>
           <h4>HTML</h4>
           <small>Experienced</small>
@@ -21,7 +44,7 @@ const experience = () => {
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiCss3 className='experience__details-icon'/>
           <div>
           <h4>CSS</h4>
           <small>Intermediate</small>
@@ -29,7 +52,7 @@ const experience = () => {
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiJavascript className='experience__details-icon'/>
           <div>
           <h4>Javascript</h4>
           <small>Intermediate</small>
@@ -37,7 +60,7 @@ const experience = () => {
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiPython className='experience__details-icon'/>
           <div>
           <h4>Python</h4>
           <small>Intermidiate</small>
@@ -45,7 +68,7 @@ const experience = () => {
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiCplusplus className='experience__details-icon'/>
           <div>
           <h4>C++</h4>
           <small>Intermediate</small>
@@ -53,7 +76,15 @@ const experience = () => {
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiTypescript className='experience__details-icon'/>
+          <div>
+          <h4>Typescript</h4>
+          <small>Intermediate</small>
+          </div>
+          </article>
+
+          <article className='experience__details'>
+          <SiKotlin className='experience__details-icon'/>
           <div>
           <h4>Kotlin</h4>
           <small>Basic</small>
@@ -61,36 +92,32 @@ const experience = () => {
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <TbBrandPhp className='experience__details-icon'/>
           <div>
           <h4>PHP</h4>
           <small>Basic</small>
           </div>
           </article>
 
-          <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
-          <div>
-          <h4>Typescript</h4>
-          <small>Basic</small>
-          </div>
-          </article>
+          
         </div>
         </div>
 
         <div className='experience__content'>
           <h3>Technologies</h3>
+          
           <div className='experience__content2'>
+          
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiAngular className='experience__details-icon'/>
           <div>
-          <h4>Node.js</h4>
-          <small>Basic</small>
+          <h4>Angular.js</h4>
+          <small>Intermediate</small>
           </div>
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiReact className='experience__details-icon'/>
           <div>
           <h4>React.js</h4>
           <small>Intermediate</small>
@@ -98,7 +125,7 @@ const experience = () => {
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiGit className='experience__details-icon'/>
           <div>
           <h4>Git</h4>
           <small>Intermediate</small>
@@ -106,20 +133,38 @@ const experience = () => {
           </article>
 
           <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
+          <SiGithub className='experience__details-icon'/>
+          <div>
+          <h4>Github</h4>
+          <small>Intermediate</small>
+          </div>
+          </article>
+
+          <article className='experience__details'>
+          <SiNodedotjs className='experience__details-icon'/>
+          <div>
+          <h4>Node.js</h4>
+          <small>Basic</small>
+          </div>
+          </article>
+
+          <article className='experience__details'>
+          <SiDocker className='experience__details-icon'/>
+          <div>
+          <h4>Docker</h4>
+          <small>Basic</small>
+          </div>
+          </article>
+
+
+          <article className='experience__details'>
+          <SiJquery className='experience__details-icon'/>
           <div>
           <h4>Jquery</h4>
           <small>Basic</small>
           </div>
           </article>
 
-          <article className='experience__details'>
-          <GoVerified className='experience__details-icon'/>
-          <div>
-          <h4>Github</h4>
-          <small>Intermediate</small>
-          </div>
-          </article>
           </div>
 
 
